bc4d1351e87d5d67ae1fb549cc9ff318b8a5c196
Daria Ośka-Ossowska <daria.oska@gmail.com>
2023-01-15 23:05:02 +0100

    Added: step 1, 2, 3, step 4: schema set

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 const app = require('./app')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+	console.log('\nServer running. Use our API on port: 3000')
 })
+
+dotenv.config()
+
+mongoose
+	.connect(process.env.DB_HOST, {
+		// useNewUrlParser: true,
+		// useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('\nDatabase connection successful\n')
+	})
+	.catch(error => {
+		console.log(error.message)
+		process.exit(1)
+	})
+
+function signalHandler() {
+	console.log('Database disconnected')
+	mongoose.disconnect()
+
+}
+process.on('SIGINT', signalHandler)
+
+
+
