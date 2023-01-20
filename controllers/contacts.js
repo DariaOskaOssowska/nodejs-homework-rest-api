4f46d1c7caf1833188ade32222e2a18f00f4bbf7
Daria Ośka-Ossowska <daria.oska@gmail.com>
2023-01-20 16:07:11 +0100

    Del: logs

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,14 +2,12 @@ const service = require('../service')
 const { contactValidator } = require('./../utils/validators/validator')
 
 const getAll = async (req, res) => {
-	console.log('sruuuuuuu')
 	const contacts = await service.getAllContacts()
 	console.log('contacts: ', contacts)
 	res.status(200).json(contacts)
 }
 
 const getById = async (req, res) => {
-	console.log('srrrru', req.params)
 	const { contactId } = req.params
 	const contact = await service.getContactById(contactId)
 	if (contact) {
@@ -20,7 +18,6 @@ const getById = async (req, res) => {
 }
 
 const addContact = async (req, res, next) => {
-	console.log('psotek')
 	const { name, email, phone } = req.body
 	try {
 		const result = await service.createContact({ name, email, phone })
@@ -33,19 +30,16 @@ const addContact = async (req, res, next) => {
 
 const updateContact = async (req, res, next) => {
 	try {
-		console.log('hej')
 		const { error } = contactValidator(req.body)
 		if (error) return res.status(400).json({ message: error.details[0].message })
 		const { name, email, phone } = req.body
 		const { contactId } = req.params
 		const fields = req.body
-		console.log('sru body', req.body)
-		console.log('contact id', contactId)
 		if (!name && !email && !phone) {
 			res.status(400).json({ message: 'missing fields' })
 		}
         const contact = await service.updateContact(contactId, fields)
-        console.log(contact, 'contact');
+    
 		if (contact) {
 			res.status(200).json(contact)
 		} else {
